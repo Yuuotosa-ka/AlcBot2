2283ebbdc07fce01edff1455cd3d2f1d355e0470
Yuuotosa-ka <92831727+Yuuotosa-ka@users.noreply.github.com>
2022-01-28 14:30:49 -0600

    Add files via upload

---
+++ src/menu.js
@@ -0,0 +1,480 @@
+ 
+ ​const​ ​menu​ ​=​ ​(​hr​,​ ​prema​,​ ​pushname​)​ ​=>​ ​{ 
+ ​prefix​ ​=​ ​'/' 
+ ​ ​return​ ​` 
+ ​╭═══════════════⊷ 
+ ​╰╮ ⟨❗⟩️ 𝖨𝗇𝖿𝗈 𝖡𝗈𝗍 ⟨❗⟩ 
+ ​╭┤⋟ Oᴡɴᴇʀ『 *Yuu @* 』 
+ ​┃│⋟ Pʀᴇғɪx:『 *​${​prefix​}​* 』 
+ ​┃╰═════════════⊷ 
+ ​╰╦═════「★」═════⊷ 
+ ​╭┤⟨👤⟩ T𝗎 𝖯𝖾𝗋𝖿𝗂𝗅 ⟨👤⟩ 
+ ​┃╞═════════════⊷ 
+ ​┃│⋟ Tɪᴘᴏ ᴅᴇ ᴜsᴜᴀʀɪᴏ: ​${​prema​}​         
+ ​┃│⋟ Nᴏᴍʙʀᴇ: ​${​pushname​}​   
+ ​┃╰═════════════⊷ 
+ ​╰╦═════「★」═════⊷ 
+ ​╭┤⟨🤑⟩ 𝖭ᴜᴇᴠᴏ 𝖬𝖾𝗇𝗎 ⟨🤑⟩ 
+ ​┃╞═════════════⊷ 
+ ​┃│⋟ *Sᴛɪᴄᴋᴇʀs* 
+ ​├┤⟨👾⟩ 𐌼ᴇɴᴜ ᴅᴇ sᴛɪᴄᴋᴇʀs ⟨👾⟩ 
+ ​┃┝━━━───• 
+ ​┃│࿊⃟️ ​${​prefix​}​exif 
+ ​┃│࿊⃟ ​${​prefix​}​robar 
+ ​┃│࿊⃟ ​${​prefix​}​sticker 
+ ​┃│ 
+ ​├┤⟨🔞⟩ 𐌼ᴇɴᴜ ᴅᴇ sᴛɪᴄᴋᴇʀ +18 ⟨🔞⟩ 
+ ​┃│࿊⃟ ​${​prefix​}​stickcum 
+ ​┃│࿊⃟ ​${​prefix​}​stickhentai 
+ ​┃│࿊⃟ ​${​prefix​}​stickfeetg 
+ ​┃│࿊⃟ ​${​prefix​}​stickblowjob 
+ ​┃│࿊⃟ ​${​prefix​}​stickpussy 
+ ​┃│࿊⃟ ​${​prefix​}​stickneko 
+ ​┃╰━━━─── • ──━━━━ 
+ ​┃ 
+ ​┃╭ ⋟ *Gʀᴜᴘᴏs* 
+ ​├┤⟨️💳⟩ 𐌼ᴇɴᴜ ᴅᴇ ᴀᴅᴍs⟨💳⟩ 
+ ​┃┝━━━───• 
+ ​┃│࿊⃟️ ​${​prefix​}​kick 
+ ​┃│࿊⃟️ ​${​prefix​}​kick2 
+ ​┃│࿊⃟️ ​${​prefix​}​delttt 
+ ​┃│࿊⃟️ ​${​prefix​}​getpp 
+ ​┃│࿊⃟️ ​${​prefix​}​opengc 
+ ​┃│࿊⃟ ​${​prefix​}​closegc 
+ ​┃│࿊⃟️ ​${​prefix​}​linkgrup 
+ ​┃│࿊⃟️ ​${​prefix​}​listadmin 
+ ​┃│࿊⃟️ ​${​prefix​}​listonline 
+ ​┃│࿊⃟ ​${​prefix​}​getdeskgc 
+ ​┃│࿊⃟️ ​${​prefix​}​resetlinkgc 
+ ​┃│࿊⃟️ ​${​prefix​}​nsfw「 1/0 」 
+ ​┃│࿊⃟️ ​${​prefix​}​getbio「 @ 」 
+ ​┃│࿊⃟️ ​${​prefix​}​getpict「 @ 」 
+ ​┃│࿊⃟️ ​${​prefix​}​demote「 @ 」 
+ ​┃│࿊⃟️ ​${​prefix​}​hidetag「 txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​sticktag「 txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​promote「 @ 」 
+ ​┃│࿊⃟️ ​${​prefix​}​antilink「 1/0 」 
+ ​┃│࿊⃟ ​${​prefix​}​tictactoe「 @ 」 
+ ​┃│࿊⃟️ ​${​prefix​}​add「 numero 」 
+ ​┃│࿊⃟️ ​${​prefix​}​sider「 msgbot 」 
+ ​┃│࿊⃟️ ​${​prefix​}​setdeskgc「 txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​setnamegc「 txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​kontak「 @tag|txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​kontag「 @tag|txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​creategrup「 txt|@ 」 
+ ​┃│࿊⃟️ ​${​prefix​}​totag「 img/vid/stick 」 
+ ​┃╰━━━─── • ──━━━━ 
+ ​┃ 
+ ​┃╭ ⋟ *ɪᴍᴀɢᴇɴᴇs* 
+ ​├┤⟨📷⟩ Rᴀɴᴅᴏᴍ ɪᴍɢ ⟨📷⟩ 
+ ​┃┝━━━───• 
+ ​┃│࿊⃟ ​${​prefix​}​lisa 
+ ​┃│࿊⃟ ​${​prefix​}​rose 
+ ​┃│࿊⃟ ​${​prefix​}​jisso 
+ ​┃│࿊⃟ ​${​prefix​}​jenny 
+ ​┃│࿊⃟ ​${​prefix​}​china 
+ ​┃│࿊⃟ ​${​prefix​}​japon 
+ ​┃│࿊⃟️ ​${​prefix​}​corea 
+ ​┃│࿊⃟ ​${​prefix​}​brasil 
+ ​┃│࿊⃟ ​${​prefix​}​badgirl 
+ ​┃│࿊⃟️ ​${​prefix​}​muslos 
+ ​┃│࿊⃟ ​${​prefix​}​say-cat 
+ ​┃│࿊⃟️ ​${​prefix​}​Malasia 
+ ​┃│࿊⃟️ ​${​prefix​}​vietnam 
+ ​┃│࿊⃟️ ​${​prefix​}​indonesia 
+ ​┃│࿊⃟ ​${​prefix​}​thailandia 
+ ​┃│࿊⃟ ​${​prefix​}​random-girls 
+ ​┃│࿊⃟ ​${​prefix​}​ari-gameplays 
+ ​┃╰━━━─── • ──━━━━ 
+ ​┃ 
+ ​┃╭ ⋟ *Cʀᴇᴀᴅᴏʀ* 
+ ​├┤⟨☣️⟩ 𐌼ᴇɴᴜ ժeʟ 𝖢𝗋e𝖺𝖽𝗈𝗋 ⟨☣️⟩ 
+ ​┃┝━━━─── • ──━━━━ 
+ ​┃│࿊⃟ ​${​prefix​}​exif 
+ ​┃│࿊⃟️ ​${​prefix​}​self 
+ ​┃│࿊⃟️ ​${​prefix​}​leave 
+ ​┃│࿊⃟️ ​${​prefix​}​mute 
+ ​┃│࿊⃟️ ​${​prefix​}​public 
+ ​┃│࿊⃟️ ​${​prefix​}​online 
+ ​┃│࿊⃟ ​${​prefix​}​restart 
+ ​┃│࿊⃟ ​${​prefix​}​clearall 
+ ​┃│࿊⃟ ​${​prefix​}​delchat 
+ ​┃│࿊⃟ ​${​prefix​}​unmute 
+ ​┃│࿊⃟ ​${​prefix​}​demoteall 
+ ​┃│࿊⃟️ ​${​prefix​}​promoteall 
+ ​┃│࿊⃟️ ​${​prefix​}​q「 msg 」 
+ ​┃│࿊⃟️ ​${​prefix​}​setbio「 txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​offline「 txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​setname「 txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​setprofile「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​unirse「 link gp 」 
+ ​┃│࿊⃟ ​${​prefix​}​delcmd「 sticker 」 
+ ​┃│࿊⃟️ ​${​prefix​}​addcmd「 sticker 」 
+ ​┃│࿊⃟ ​${​prefix​}​tobc「 stick/audio 」 
+ ​┃│࿊⃟ ​${​prefix​}​bc「 txt/gif/img/vid 」 
+ ​┃│࿊⃟ ​${​prefix​}​autorespon「 on/off 」 
+ ​┃╰━━━─── • ──━━━━ 
+ ​┃ 
+ ​┃╭ ⋟ *Dᴇsᴄᴀʀɢᴀs* 
+ ​├┤⟨📥⟩ 𐌼ᴇɴᴜ ᴅᴇ ᴅᴇsᴄᴀʀɢᴀs ⟨📥⟩ 
+ ​┃┝━━━───• 
+ ​┃│࿊⃟ ​${​prefix​}​mediafire 
+ ​┃│࿊⃟ ​${​prefix​}​play 
+ ​┃│࿊⃟️ ​${​prefix​}​ytmp3 
+ ​┃│࿊⃟ ​${​prefix​}​ytmp4 
+ ​┃│࿊⃟️ ​${​prefix​}​igdl 
+ ​┃│࿊⃟️ ​${​prefix​}​pinterest 
+ ​┃│࿊⃟ ​${​prefix​}​facebook 
+ ​┃│࿊⃟️ ​${​prefix​}​tiktok 
+ ​┃╰━━━─── • ──━━━━ 
+ ​┃ 
+ ​┃╭ ⋟ *Aɴɪᴍᴇs ɴsғᴡ* 
+ ​├┤⟨📂⟩ 𐌼ᴇɴᴜ +18 ⟨📂⟩ 
+ ​┃┝━━━───• 
+ ​┃│࿊⃟ ​${​prefix​}​ass 
+ ​┃│࿊⃟ ​${​prefix​}​ahegao 
+ ​┃│࿊⃟ ​${​prefix​}​bdsm 
+ ​┃│࿊⃟ ​${​prefix​}​blowjob 
+ ​┃│࿊⃟ ​${​prefix​}​cuckold 
+ ​┃│࿊⃟ ​${​prefix​}​cum 
+ ​┃│࿊⃟ ​${​prefix​}​ero 
+ ​┃│࿊⃟ ​${​prefix​}​femdom 
+ ​┃│࿊⃟ ​${​prefix​}​foot 
+ ​┃│࿊⃟ ​${​prefix​}​gangbang 
+ ​┃│࿊⃟ ​${​prefix​}​glasses 
+ ​┃│࿊⃟ ​${​prefix​}​hentai 
+ ​┃│࿊⃟ ​${​prefix​}​jahy 
+ ​┃│࿊⃟ ​${​prefix​}​masturbation 
+ ​┃│࿊⃟ ​${​prefix​}​orgy 
+ ​┃│࿊⃟ ​${​prefix​}​panties 
+ ​┃│࿊⃟ ​${​prefix​}​pussy 
+ ​┃│࿊⃟ ​${​prefix​}​thighs 
+ ​┃│࿊⃟ ​${​prefix​}​yuri 
+ ​┃│࿊⃟ ​${​prefix​}​neko4 
+ ​┃│࿊⃟ ​${​prefix​}​yuri2 
+ ​┃│࿊⃟️ ​${​prefix​}​trapnime 
+ ​┃│࿊⃟ ​${​prefix​}​yaoi 
+ ​┃│࿊⃟ ​${​prefix​}​neko3 
+ ​┃╰━━━─── • ──━━━━ 
+ ​┃ 
+ ​┃╭ ⋟ *Vᴀʀɪᴀᴅᴏ* 
+ ​├┤⟨🍃⟩ 𐌼ᴇɴᴜ ᴠᴀʀɪᴀᴅᴏ  ⟨🍃⟩ 
+ ​┃┝━━━───• 
+ ​┃│࿊⃟️ ​${​prefix​}​reto 
+ ​┃│࿊⃟ ​${​prefix​}​simi 
+ ​┃│࿊⃟ ​${​prefix​}​slots 
+ ​┃│࿊⃟ ​${​prefix​}​toimg 
+ ​┃│࿊⃟️ ​${​prefix​}​tagme 
+ ​┃│࿊⃟ ​${​prefix​}​sticker 
+ ​┃│࿊⃟️ ​${​prefix​}​jadibot 
+ ​┃│࿊⃟ ​${​prefix​}​verdad 
+ ​┃│࿊⃟️ ​${​prefix​}​listgrup 
+ ​┃│࿊⃟ ​${​prefix​}​test-gay 
+ ​┃│࿊⃟ ​${​prefix​}​test-lesbi 
+ ​┃│࿊⃟ ​${​prefix​}​test-puta 
+ ​┃│࿊⃟ ​${​prefix​}​stopjadibot 
+ ​┃│࿊⃟️ ​${​prefix​}​attp「 txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​tag「 núm 」 
+ ​┃│࿊⃟️ ​${​prefix​}​tts es「 txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​balik「 audio 」 
+ ​┃│࿊⃟ ​${​prefix​}​bass「 audio 」 
+ ​┃│࿊⃟️ ​${​prefix​}​robar「 txt|txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​robot「 audio 」 
+ ​┃│࿊⃟ ​${​prefix​}​tomp4「 stick 」 
+ ​┃│࿊⃟ ​${​prefix​}​baileys「 msg 」 
+ ​┃│࿊⃟️ ​${​prefix​}​colong「 stick 」 
+ ​┃│࿊⃟️ ​${​prefix​}​tomp3「 video 」 
+ ​┃│࿊⃟️ ​${​prefix​}​chat「 num|txt 」 
+ ​┃│࿊⃟️ ​${​prefix​}​semoji「 emoji 」 
+ ​┃│࿊⃟ ​${​prefix​}​checkbin「 bin 」 
+ ​┃│࿊⃟️ ​${​prefix​}​gemuk「 audio 」 
+ ​┃│࿊⃟️ ​${​prefix​}​detikvn「 audio 」 
+ ​┃│࿊⃟ ​${​prefix​}​encode-64「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​decode-64「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​encode-32「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​decode-32「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​detikvideo「 vid 」 
+ ​┃│࿊⃟ ​${​prefix​}​readmore「 txt|txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​getcaption「 msg 」 
+ ​┃│࿊⃟️ ​${​prefix​}​sharelock「 txt|txt 」 
+ ​┃╰━━━─── • ──━━━━ 
+ ​┃ 
+ ​┃╭ ⋟ *Aɴɪᴍᴇ* 
+ ​├┤⟨⛩️⟩ 𐌼ᴇɴᴜ ᴅᴇ ᴀɴɪᴍᴇs ⟨⛩️⟩ 
+ ​┃┝━━━───• 
+ ​┃│࿊⃟ ​${​prefix​}​wallpaper 
+ ​┃│࿊⃟ ​${​prefix​}​wallpaper2 
+ ​┃│࿊⃟️ ​${​prefix​}​padoru 
+ ​┃│࿊⃟ ​${​prefix​}​waifu 
+ ​┃│࿊⃟️ ​${​prefix​}​megu 
+ ​┃│࿊⃟️ ​${​prefix​}​megu2 
+ ​┃│࿊⃟ ​${​prefix​}​neko 
+ ​┃│࿊⃟️ ​${​prefix​}​neko2 
+ ​┃│࿊⃟️ ​${​prefix​}​loli 
+ ​┃│࿊⃟ ​${​prefix​}​loli2 
+ ​┃│࿊⃟️ ​${​prefix​}​naruto 
+ ​┃│࿊⃟ ​${​prefix​}​sasuke 
+ ​┃│࿊⃟ ​${​prefix​}​hinata 
+ ​┃│࿊⃟ ​${​prefix​}​sakura 
+ ​┃│࿊⃟ ​${​prefix​}​kanna 
+ ​┃│࿊⃟ ​${​prefix​}​shoto 
+ ​┃│࿊⃟️ ​${​prefix​}​akiyama 
+ ​┃│࿊⃟ ​${​prefix​}​anna 
+ ​┃│࿊⃟️ ​${​prefix​}​misaki 
+ ​┃│࿊⃟️ ​${​prefix​}​boruto 
+ ​┃│࿊⃟ ​${​prefix​}​chitoge 
+ ​┃│࿊⃟️ ​${​prefix​}​deidara 
+ ​┃│࿊⃟️ ​${​prefix​}​doraemon 
+ ​┃│࿊⃟ ​${​prefix​}​elaina 
+ ​┃│࿊⃟️ ​${​prefix​}​emilia 
+ ​┃│࿊⃟ ​${​prefix​}​erza 
+ ​┃│࿊⃟ ​${​prefix​}​rias 
+ ​┃│࿊⃟ ​${​prefix​}​hestia 
+ ​┃│࿊⃟ ​${​prefix​}​inori 
+ ​┃│࿊⃟ ​${​prefix​}​isuzu 
+ ​┃│࿊⃟️ ​${​prefix​}​itachi 
+ ​┃│࿊⃟ ​${​prefix​}​kaga 
+ ​┃│࿊⃟️ ​${​prefix​}​kagura 
+ ​┃│࿊⃟️ ​${​prefix​}​kakashi 
+ ​┃│࿊⃟ ​${​prefix​}​kaori 
+ ​┃│࿊⃟️ ​${​prefix​}​kaneki 
+ ​┃│࿊⃟️ ​${​prefix​}​kotori 
+ ​┃│࿊⃟ ​${​prefix​}​kurumi 
+ ​┃│࿊⃟️ ​${​prefix​}​madara 
+ ​┃│࿊⃟ ​${​prefix​}​mikasa 
+ ​┃│࿊⃟ ​${​prefix​}​miku 
+ ​┃│࿊⃟ ​${​prefix​}​minato 
+ ​┃│࿊⃟ ​${​prefix​}​onepiece 
+ ​┃│࿊⃟ ​${​prefix​}​nezuko 
+ ​┃│࿊⃟️ ​${​prefix​}​pokemon 
+ ​┃│࿊⃟ ​${​prefix​}​sagiri 
+ ​┃│࿊⃟️ ​${​prefix​}​shina 
+ ​┃│࿊⃟️ ​${​prefix​}​shinka 
+ ​┃│࿊⃟ ​${​prefix​}​shota 
+ ​┃│࿊⃟️ ​${​prefix​}​shizuka 
+ ​┃│࿊⃟️ ​${​prefix​}​touka 
+ ​┃│࿊⃟ ​${​prefix​}​tsunade 
+ ​┃╰━━━─── • ──━━━━ 
+ ​┃ 
+ ​┃╭ ⋟ *ᴇᴅɪᴛ ɪᴍɢ* 
+ ​├┤⟨🖼️⟩ ᴇᴅɪᴛᴀʀ ɪᴍᴀɢᴇɴ ⟨🖼️⟩ 
+ ​┃┝━━━───• 
+ ​┃│࿊⃟ ​${​prefix​}​horny 
+ ​┃│࿊⃟ ​${​prefix​}​triggered「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​gay「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​glass「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​passed「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​jail「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​comrade「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​green「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​blue「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​sepia「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​wasted「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​greyscale「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​blurple2「 img 」  
+ ​┃│࿊⃟ ​${​prefix​}​blurple「 img 」  
+ ​┃│࿊⃟ ​${​prefix​}​red「 img 」  
+ ​┃│࿊⃟ ​${​prefix​}​invertgreyscale「 img 」  
+ ​┃│࿊⃟ ​${​prefix​}​invert「 img 」 
+ ​┃│࿊⃟ ​${​prefix​}​simp「 @ 」 
+ ​┃│࿊⃟ ​${​prefix​}​tweet「 txt1|txt2|txt3 」 
+ ​┃│࿊⃟ ​${​prefix​}​ytcomment「 txt|txt|txt 」 
+ ​┃╰━━━─── • ──━━━━ 
+ ​┃ 
+ ​┃╭ ⋟ *Lᴏɢᴏs* 
+ ​├┤⟨❄️⟩ 𐌼ᴇɴᴜ ᴅᴇ ʟᴏɢᴏs ⟨❄️⟩ 
+ ​┃┝━━━───• 
+ ​├┤⟨🍃⟩ ᴛᴇxᴛᴘʀᴏ ⟨🍃⟩ 
+ ​┃│ 
+ ​┃│࿊⃟ ​${​prefix​}​blackpink「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​rainbow2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​water_pipe「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​halloween「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​sketch「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​sircuit「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​discovery「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​metallic2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​fiction「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​demon「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​transformer「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​berry「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​thunder「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​magma「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​3dstone「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​neon「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glitch「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​harry_potter「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​embossed「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​broken「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​papercut「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​gradient「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glossy「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​watercolor「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​multicolor「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​neon_devil「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​underwater「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​bear「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​wonderfulg「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​christmas「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​neon_light「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​snow「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​cloudsky「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​luxury2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​gradient2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​summer「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​writing「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​engraved「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​summery「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​3dglue「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​metaldark「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​neonlight「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​oscar「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​minion「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​holographic「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​purple「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glossyb「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​deluxe2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glossyc「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​fabric「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​neonc「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​newyear「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​newyear2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​metals「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​xmas「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​blood「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​darkg「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​joker「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​wicker「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​natural「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​firework「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​skeleton「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​balloon「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​balloon2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​balloon3「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​balloon4「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​balloon5「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​balloon6「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​balloon7「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​steel「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​gloss「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​denim「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​decorate「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​decorate2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​peridot「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​rock「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glass「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glass2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glass3「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glass4「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glass5「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glass6「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glass7「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glass8「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​captain_as2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​robot「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​equalizer「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​toxic「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​sparkling「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​sparkling2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​sparkling3「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​sparkling4「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​sparkling5「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​sparkling6「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​sparkling7「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​decorative「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​chocolate「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​strawberry「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​koifish「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​bread「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​matrix「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​blood2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​neonligth2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​thunder2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​3dbox「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​neon2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​roadw「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​bokeh「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​gneon「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​advanced「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​dropwater「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​wall「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​chrismast「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​honey「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​drug「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​marble「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​marble2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​ice「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​juice「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​rusty「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​abstra「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​biscuit「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​wood「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​scifi「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​metalr「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​purpleg「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​shiny「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​jewelry「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​jewelry2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​jewelry3「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​jewelry4「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​jewelry5「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​jewelry6「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​jewelry7「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​jewelry8「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​metalh「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​golden「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glitter「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glitter2「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glitter3「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glitter4「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glitter5「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glitter6「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​glitter7「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​metale「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​carbon「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​candy「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​metalb「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​gemb「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​3dchrome「 txt 」 
+ ​┃│࿊⃟ ​${​prefix​}​metalb2「 txt 」 
+ ​┃│ 
+ ​├┤⟨🌿⟩ ᴛᴇxᴛᴘʀᴏ2 ⟨🌿️⟩ 
+ ​┃┝━━━───• 
+ ​┃│࿊⃟ ​${​prefix​}​halloween2「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​horror「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​game8bit「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​layered「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​glitch2「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​coolg「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​coolwg「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​realistic「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​space3d「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​gtiktok「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​stone「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​marvel「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​marvel2「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​pornhub「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​avengers「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​metalr「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​metalg「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​metalg2「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​halloween2「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​lion「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​wolf_bw「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​wolf_g「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​ninja「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​3dsteel「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​horror2「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​lava「 txt1|txt2 」 
+ ​┃│࿊⃟ ​${​prefix​}​bagel「 txt1|txt2 」 
+ ​┃╰━━━─── • ──━━━━ 
+ ​┃╰┼═════════════⊷ 
+ ​╭┤️⟨⚠️️⟩ 𝙸ɴғᴏʀᴍᴀᴄɪᴏɴ ⟨⚠️️⟩ 
+ ​┃│ 
+ ​┃│✙◗ᴇɴʟᴀᴄᴇs = ʙᴀɴ 
+ ​┃│✙◗sᴘᴀᴍ = ʙᴀɴ 
+ ​┃│✙◗ʙᴏᴛ ʙʏ =  ᴀʟᴄ 
+ ​┃╰═════════════⊷ 
+ ​╰─━━━─── • ───━━━━❋ཻུ۪۪⸙` 
+ ​} 
+ ​exports​.​menu​ ​=​ ​menu
